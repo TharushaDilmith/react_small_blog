7fec595f43fc7ca0c937753209e88e3f0fa94140
IT19153964 <tharushadilmith99@gmail.com>
2021-05-29 02:29:13 +0530

    commit Home component and Navbar

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return ( 
+
+        <div className="home">
+            <h2>Home page</h2>
+        </div>
+     );
+}
+ 
+export default Home;
+
+<div className="home"></div>
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="nacbar">
+            <h1>Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
